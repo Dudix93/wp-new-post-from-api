337246972709e1daa4905a6dd55ca740e80f41d8
Michał Dodot <dodo45693@gmail.com>
2020-04-13 15:56:15 +0100

    Update post-from-api.php

--- post-from-api.php
+++ post-from-api.php
@@ -6,7 +6,6 @@ Version:     1.0
 Author:      Michal D
 */
 function addPost($title, $content) {
-	global $user_ID;
 	$new_post = array(
 	'post_title' => $title,
 	'post_content' => $content,
@@ -19,15 +18,40 @@ function addPost($title, $content) {
 	$post_id = wp_insert_post($new_post);
 }
 
-$url = 'http://127.0.0.1:8000/api/posts';
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-$data = curl_exec($curl);
-curl_close($curl);
-$posts = json_decode($data, true);
-
-foreach($posts as $post) {
-	echo $post['title'].PHP_EOL;
-	addPost($post['title'],
-		$post['content']);
+function ifPostExists( $title, $content = '') {
+	$post_title   = wp_unslash( sanitize_post_field( 'post_title', $title, 0, 'db' ) );
+    $post_content = wp_unslash( sanitize_post_field( 'post_content', $content, 0, 'db' ) );
+ 
+    $query = "SELECT ID FROM $wpdb->posts WHERE 1=1";
+    $args  = array();
+ 
+    if ( ! empty( $title ) ) {
+        $query .= ' AND post_title = %s';
+        $args[] = $post_title;
+    }
+ 
+    if ( ! empty( $content ) ) {
+        $query .= ' AND post_content = %s';
+        $args[] = $post_content;
+    }
+ 
+    return 0;
 }
+
+function addPostsFromAPI() {
+	global $user_ID;
+	global $wpdb;
+	$url = 'http://127.0.0.1:8000/api/posts';
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	$data = curl_exec($curl);
+	curl_close($curl);
+	$posts = json_decode($data, true);
+
+	foreach($posts as $post) {
+		if (!ifPostExists($post['title'], $post['content'])) {
+			addPost($post['title'],	$post['content']);
+		}
+	}
+}
+add_action('init', 'addPostsFromAPI');
