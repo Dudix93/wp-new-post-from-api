e9c6a0eeadfb3704a0a84487e5adefa22b7abc79
Michał Dodot <dodo45693@gmail.com>
2020-04-13 16:24:17 +0100

    Update post-from-api.php

--- post-from-api.php
+++ post-from-api.php
@@ -19,39 +19,32 @@ function addPost($title, $content) {
 }
 
 function ifPostExists( $title, $content = '') {
-	$post_title   = wp_unslash( sanitize_post_field( 'post_title', $title, 0, 'db' ) );
-    $post_content = wp_unslash( sanitize_post_field( 'post_content', $content, 0, 'db' ) );
- 
-    $query = "SELECT ID FROM $wpdb->posts WHERE 1=1";
-    $args  = array();
- 
-    if ( ! empty( $title ) ) {
-        $query .= ' AND post_title = %s';
-        $args[] = $post_title;
-    }
- 
-    if ( ! empty( $content ) ) {
-        $query .= ' AND post_content = %s';
-        $args[] = $post_content;
-    }
- 
-    return 0;
+    global $wpdb;
+	$query = $wpdb->prepare(
+        'SELECT ID FROM ' . $wpdb->posts . '
+        WHERE post_title = %s
+		AND post_content = %s',
+        $title,
+		$content
+    );
+    $wpdb->query( $query );
+	return $wpdb->num_rows;
 }
 
 function addPostsFromAPI() {
 	global $user_ID;
-	global $wpdb;
 	$url = 'http://127.0.0.1:8000/api/posts';
 	$curl = curl_init($url);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 	$data = curl_exec($curl);
 	curl_close($curl);
 	$posts = json_decode($data, true);
-
-	foreach($posts as $post) {
+	if (isset($posts)) {
+		foreach($posts as $post) {
 		if (!ifPostExists($post['title'], $post['content'])) {
 			addPost($post['title'],	$post['content']);
 		}
+	}
 	}
 }
 add_action('init', 'addPostsFromAPI');
